79a1bfbd98034f6d7a792fedf3e9660a03e46d7b
jeff <jeff@infograb.net>
2023-11-30 20:15:45 +0900

    extract email info as varriables, add movement threshold

--- fire-detector.py
+++ fire-detector.py
@@ -1,36 +1,53 @@
-
 import cv2
 import numpy as np
-import smtplib
 import playsound
 import threading
+import smtplib
+import os
+
+# Environment variables
+SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
+RECIPIENT_EMAIL = os.getenv("RECIPIENT_EMAIL")
+
+# Validate environment variables
+if not SENDER_EMAIL or not SENDER_PASSWORD or not RECIPIENT_EMAIL:
+    raise ValueError("Please set the required environment variables.")
+
 
 Alarm_Status = False
 Email_Status = False
 Fire_Reported = 0
+frame_check = 10
+
 
 def play_alarm_sound_function():
-	while True:
-		playsound.playsound('alarm-sound.mp3',True)
+    while True:
+        playsound.playsound("alarm-sound.mp3", True)
+
 
 def send_mail_function():
-
-    recipientEmail = "Enter_Recipient_Email"
+    recipientEmail = RECIPIENT_EMAIL
     recipientEmail = recipientEmail.lower()
 
     try:
-        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server = smtplib.SMTP("smtp.gmail.com", 587)
         server.ehlo()
         server.starttls()
-        server.login("Enter_Your_Email (System Email)", 'Enter_Your_Email_Password (System Email')
-        server.sendmail('Enter_Your_Email (System Email)', recipientEmail, "Warning A Fire Accident has been reported on ABC Company")
+        server.login(SENDER_EMAIL, SENDER_PASSWORD)
+        server.sendmail(
+            SENDER_EMAIL,
+            recipientEmail,
+            "Subject: Fire Alert!\n\nWarning! A fire has been detected. Please check your security camera footage.",
+        )
         print("sent to {}".format(recipientEmail))
         server.close()
     except Exception as e:
-    	print(e)
+        print(e)
 
 
-video = cv2.VideoCapture("video_file") # If you want to use webcam use Index like 0,1.
+video = cv2.VideoCapture("fire.mp4")
+first_frame = None
 
 while True:
     (grabbed, frame) = video.read()
@@ -38,6 +55,17 @@ while True:
         break
 
     frame = cv2.resize(frame, (960, 540))
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    if first_frame is None:
+        first_frame = gray
+        continue
+
+    frame_delta = cv2.absdiff(first_frame, gray)
+    thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    movement = np.sum(thresh) / 255
 
     blur = cv2.GaussianBlur(frame, (21, 21), 0)
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
@@ -48,28 +76,23 @@ while True:
     upper = np.array(upper, dtype="uint8")
 
     mask = cv2.inRange(hsv, lower, upper)
-
     output = cv2.bitwise_and(frame, hsv, mask=mask)
-
     no_red = cv2.countNonZero(mask)
 
-    if int(no_red) > 15000:
+    if int(no_red) > 15000 and movement > frame_check:
         Fire_Reported = Fire_Reported + 1
 
     cv2.imshow("output", output)
 
     if Fire_Reported >= 1:
+        if not Alarm_Status:
+            threading.Thread(target=play_alarm_sound_function).start()
+            Alarm_Status = True
+        if not Email_Status:
+            threading.Thread(target=send_mail_function).start()
+            Email_Status = True
 
-    	if Alarm_Status == False:
-    		threading.Thread(target=play_alarm_sound_function).start()
-    		Alarm_Status = True
-
-    	if Email_Status == False:
-    		threading.Thread(target=send_mail_function).start()
-    		Email_Status = True
-
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cv2.destroyAllWindows()
